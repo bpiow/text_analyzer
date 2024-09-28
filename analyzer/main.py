fbd55b0ca804ee956f87a2b61866d21c2e1cb13b
bpiow <batiw95@gmail.com>
2024-09-28 11:56:19 +0200

    ELK Stack removed for the time being

--- analyzer/main.py
+++ analyzer/main.py
@@ -2,31 +2,8 @@ from fastapi import FastAPI
 from pydantic import BaseModel
 from joblib import load
 from preprocess import preprocess_text
+
 from mongodb.save_results import save_prediction
-
-import logging
-import socket
-import json
-
-
-# Logstash configuration
-logger = logging.getLogger("analyzer")
-logger.setLevel(logging.INFO)
-
-logstash_handler = logging.StreamHandler()
-log_format = logging.Formatter(json.dumps({
-    'timestamp': '%(asctime)s',
-    'level': '%(levelname)s',
-    'message': '%(message)s',
-    'app': 'analyzer',
-    'host': socket.gethostname(),
-}))
-
-logstash_handler.setFormatter(log_format)
-logger.addHandler(logstash_handler)
-
-logger.info("application started successfully.")
-
 
 # App configuration
 model = load("Results/text_classifier.joblib")
@@ -45,6 +22,7 @@ def predict_text(input: TextInput):
     text_vector = vectorizer.transform([clean_text])
     prediction = model.predict(text_vector)
     category = ['rec.sport.hockey', 'sci.space'][prediction[0]]
+    # Save to MongoDB
     save_prediction(input.text, category)
     return {"prediction": category}
 
